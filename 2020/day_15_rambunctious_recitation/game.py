9d85933c1b5a1b5a8a512fbe751ee6099234c9b3
Konrad Kocik <konrad.kocik@gmail.com>
2022-11-11 19:30:16 +0100

    Add day 15 part 2

--- 2020/day_15_rambunctious_recitation/game.py
+++ 2020/day_15_rambunctious_recitation/game.py
@@ -1,16 +1,24 @@
 def play(final_turn, numbers):
+    last_number = numbers[-1]
+    numbers = {number: [turn, None] for turn, number in enumerate(numbers, 1)}
     turn = len(numbers) + 1
 
     while turn <= final_turn:
-        numbers.append(_get_number_age(turn, numbers[-1], numbers[:-1]))
+        print(f'Turn {turn}')
+        last_number = _get_next_number(turn, last_number, numbers)
+        _update_numbers(turn, last_number, numbers)
         turn += 1
 
-    return numbers[-1]
+    return last_number
 
 
-def _get_number_age(turn, number, numbers):
-    return (turn - 1) - _get_last_index_of(number, numbers) if number in numbers else 0
+def _get_next_number(turn, last_number, numbers):
+    if last_number not in numbers:
+        return 0
+    else:
+        number_history = numbers[last_number]
+        return (turn - 1) - number_history[0] if number_history[1] is None else number_history[0] - number_history[1]
 
 
-def _get_last_index_of(number, numbers):
-    return len(numbers) - numbers[::-1].index(number)
+def _update_numbers(turn, number, numbers):
+    numbers[number] = [turn, None] if number not in numbers else [turn, numbers[number][0]]
--- 2020/day_15_rambunctious_recitation/rambunctious_recitation.py
+++ 2020/day_15_rambunctious_recitation/rambunctious_recitation.py
@@ -3,3 +3,7 @@ from game import play
 print('\nSolving first part of puzzle')
 game_result = play(final_turn=2020, numbers=[1, 17, 0, 10, 18, 11, 6])
 print(f'Answer to first part of puzzle is: {game_result}')
+
+print('\nSolving second part of puzzle')
+game_result = play(final_turn=30000000, numbers=[1, 17, 0, 10, 18, 11, 6])
+print(f'Answer to second part of puzzle is: {game_result}')
