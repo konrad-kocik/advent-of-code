fb9f3b441016671d2047b9fe91f871af9290ae7e
Konrad Kocik <konrad.kocik@gmail.com>
2024-12-27 21:36:25 +0100

    Add 2024 day 10 part 2

--- 2024/day_10_hoof_it/solution.py
+++ 2024/day_10_hoof_it/solution.py
@@ -4,7 +4,6 @@ print('\nSolving first part of puzzle')
 sum_of_trailhead_scores = rate_trailheads('input.raw')
 print(f'Answer to first part of puzzle is: {sum_of_trailhead_scores}')
 
-
 print('\nSolving second part of puzzle')
 sum_of_trailhead_ratings = rate_trailheads('input.raw', distinct_trails=True)
 print(f'Answer to second part of puzzle is: {sum_of_trailhead_ratings}')
