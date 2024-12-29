1f24df66820516badae6093b7c3700d54802abe0
Konrad Kocik <konrad.kocik@gmail.com>
2024-12-29 13:42:38 +0100

    Add 2024 day 11 part 2

--- 2024/day_11_plutonian_pebbles/physics_defying_stones.py
+++ 2024/day_11_plutonian_pebbles/physics_defying_stones.py
@@ -1,34 +1,78 @@
 from typing import List
 
-Stones = List[int]
+
+class Stone:
+    def __init__(self, base_value: int, multiplier: int = 1):
+        self.base_value = base_value
+        self.multiplier = multiplier
+
+    def __repr__(self):
+        return str(self.base_value * self.multiplier)
+
+    @property
+    def number_engraved(self) -> int:
+        return self.base_value * self.multiplier
 
 
 def count_stones(input_file_path: str, blinks_count: int) -> int:
     stones = _examine_stones(input_file_path)
-    stones = _blink(stones, blinks_count=blinks_count)
-    return len(stones)
+    return _blink(stones, blinks_count=blinks_count)
 
 
-def _examine_stones(input_file_path: str) -> Stones:
+def _examine_stones(input_file_path: str) -> List[Stone]:
     with open(input_file_path, 'r') as file:
-        return [int(stone) for stone in file.readline().split()]
+        return [Stone(int(number)) for number in file.readline().split()]
 
 
-def _blink(stones: Stones, blinks_count: int) -> Stones:
-    for _ in range(blinks_count):
+def _blink(stones: List[Stone], blinks_count: int) -> List[Stone]:
+    zero_stones_count = 0
+
+    for blink_id in range(blinks_count):
+        print(f'Blink {blink_id + 1}')
         changed_stones = []
 
         for stone in stones:
-            if stone == 0:
-                changed_stones.append(1)
-            elif len(str(stone)) % 2 == 0:
-                stone = str(stone)
+            if stone.number_engraved == 0:
+                zero_stones_count += 1
+            elif len(str(stone.number_engraved)) % 2 == 0:
+                stone = str(stone.number_engraved)
                 left_half_of_digits, right_half_of_digits = stone[:len(stone) // 2], stone[len(stone) // 2:]
-                changed_stones.append(int(left_half_of_digits))
-                changed_stones.append(int(right_half_of_digits))
+                changed_stones.append(Stone(int(left_half_of_digits)))
+                changed_stones.append(Stone(int(right_half_of_digits)))
             else:
-                changed_stones.append(stone * 2024)
+                stone.multiplier *= 2024
+                changed_stones.append(stone)
 
         stones = changed_stones
 
-    return changed_stones
+    print(f'Basic stones count: {len(stones)}')
+
+    return len(stones) + _unfold_zero_stones(zero_stones_count, blinks_count)
+
+
+def _unfold_zero_stones(zero_stones_count: int, blinks_count: int):
+    # print(f'Zero stones count: {zero_stones_count}')
+    stones = [Stone(0)]
+    print(stones)
+
+    for blink_id in range(blinks_count):
+        changed_stones = []
+
+        for stone in stones:
+            if stone.number_engraved == 0:
+                changed_stones.append(Stone(1))
+            elif len(str(stone.number_engraved)) % 2 == 0:
+                stone = str(stone.number_engraved)
+                left_half_of_digits, right_half_of_digits = stone[:len(stone) // 2], stone[len(stone) // 2:]
+                changed_stones.append(Stone(int(left_half_of_digits)))
+                changed_stones.append(Stone(int(right_half_of_digits)))
+            else:
+                stone.multiplier *= 2024
+                changed_stones.append(stone)
+
+        stones = changed_stones
+        print(stones)
+
+    # print(f'Zero stone unfolds to: {len(stones)}')
+    # print(f'Zero stones count: {len(stones) * zero_stones_count}')
+    return len(stones) * zero_stones_count
--- 2024/day_11_plutonian_pebbles/test_physics_defying_stones.py
+++ 2024/day_11_plutonian_pebbles/test_physics_defying_stones.py
@@ -1,5 +1,6 @@
-from physics_defying_stones import count_stones
+from physics_defying_stones import count_stones, _unfold_zero_stones
 
 
 def test_count_stones():
-    assert count_stones('test_input.raw', blinks_count=6) == 22
+    # assert count_stones('test_input.raw', blinks_count=6) == 22
+    _unfold_zero_stones(1, 75)
