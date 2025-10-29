093006d400c61b71c810880a276d641cd2be8933
Konrad Kocik <konrad.kocik@nokia.com>
2025-10-29 09:48:56 +0100

    Add 2015 day 1 part 2

--- 2015/day_1_not_quite_lisp/building.py
+++ 2015/day_1_not_quite_lisp/building.py
@@ -1,6 +1,14 @@
+from typing import Optional
+
+
 def find_santas_floor(input_file_path: str) -> int:
     instructions = _get_instructions(input_file_path)
     return _calculate_floor(instructions)
+
+
+def wait_for_santa_in_basement(input_file_path: str) -> Optional[int]:
+    instructions = _get_instructions(input_file_path)
+    return _get_instruction_number_for_basement(instructions)
 
 
 def _get_instructions(input_file_path: str) -> str:
@@ -10,3 +18,15 @@ def _get_instructions(input_file_path: str) -> str:
 
 def _calculate_floor(instructions: str) -> int:
     return instructions.count('(') - instructions.count(')')
+
+
+def _get_instruction_number_for_basement(instructions: str) -> Optional[int]:
+    floor = 0
+
+    for instruction_index, instruction in enumerate(instructions, start=1):
+        floor += 1 if instruction == '(' else -1
+
+        if floor == -1:
+            return instruction_index
+
+    return None
--- 2015/day_1_not_quite_lisp/solution.py
+++ 2015/day_1_not_quite_lisp/solution.py
@@ -1,5 +1,9 @@
-from building import find_santas_floor
+from building import find_santas_floor, wait_for_santa_in_basement
 
 print('\nSolving first part of puzzle')
 floor_number = find_santas_floor('input.raw')
 print(f'Answer to first part of puzzle is: {floor_number}')
+
+print('\nSolving second part of puzzle')
+instruction_number = wait_for_santa_in_basement('input.raw')
+print(f'Answer to second part of puzzle is: {instruction_number}')
