7a698b01a920dc9c89f000380422f1c824bfaf2f
Konrad Kocik <konrad.kocik@gmail.com>
2025-11-14 19:26:18 +0100

    Add 2015 day 11 part 2

--- 2015/day_11_corporate_policy/solution.py
+++ 2015/day_11_corporate_policy/solution.py
@@ -3,3 +3,7 @@ from password_manager import change_password
 print('Solving first part of puzzle')
 new_password = change_password(old_password='hepxcrrq')
 print(f'Answer to first part of puzzle is: {new_password}')
+
+print('Solving second part of puzzle')
+new_password = change_password(old_password=new_password)
+print(f'Answer to second part of puzzle is: {new_password}')
