c0f4ba85d27ed34de9b436e9bcca132b94c0e890
Konrad Kocik <konrad.kocik@nokia.com>
2025-12-02 11:14:47 +0100

    Add 2025 day 2 part 2

--- 2025/day_2_gift_shop/database.py
+++ 2025/day_2_gift_shop/database.py
@@ -9,17 +9,38 @@ class Database:
                 for product_id in range(int(product_id_range_start), int(product_id_range_end) + 1):
                     self._product_ids.append(product_id)
 
-    def find_invalid_product_ids(self) -> list[int]:
+    def find_invalid_product_ids(self, advanced_search: bool = False) -> list[int]:
         invalid_product_ids = []
 
         for product_id in self._product_ids:
-            product_id = str(product_id)
-
-            if len(product_id) % 2 == 0:
-                product_id_head = product_id[:len(product_id) // 2]
-                product_id_tail = product_id[len(product_id) // 2:]
-
-                if product_id_head == product_id_tail:
-                    invalid_product_ids.append(int(product_id))
+            if advanced_search:
+                if self._check_if_invalid_using_advanced_mode(str(product_id)):
+                    invalid_product_ids.append(product_id)
+            else:
+                if self._check_if_invalid_using_basic_mode(str(product_id)):
+                    invalid_product_ids.append(product_id)                                    
 
         return invalid_product_ids        
+
+    def _check_if_invalid_using_advanced_mode(self, product_id: str) -> bool:
+        for split_length in range(1, len(product_id)):
+            if len(product_id) % split_length == 0:
+                product_id_to_split = product_id[:]
+                product_id_parts = set()
+
+                for _ in range(len(product_id) // split_length):
+                    product_id_parts.add(product_id_to_split[:split_length])
+                    product_id_to_split = product_id_to_split[split_length:]
+
+                if len(product_id_parts) == 1:
+                    return True
+        return False
+
+    def _check_if_invalid_using_basic_mode(self, product_id: str) -> bool:
+        if len(product_id) % 2 == 0:
+                    product_id_head = product_id[:len(product_id) // 2]
+                    product_id_tail = product_id[len(product_id) // 2:]
+
+                    if product_id_head == product_id_tail:
+                        return True
+        return False
--- 2025/day_2_gift_shop/test_database.py
+++ 2025/day_2_gift_shop/test_database.py
@@ -1,8 +1,20 @@
+from pytest import fixture
+
 from database import Database
 
 
-def test_find_invalid_product_ids():
-    database = Database()
-    database.load('test_input.raw')
+@fixture
+def database():
+    db = Database()
+    db.load('test_input.raw')
+    return db
+
+
+def test_find_invalid_product_ids(database: Database):
     invalid_product_ids = database.find_invalid_product_ids()
     assert sum(invalid_product_ids) == 1227775554
+
+
+def test_find_invalid_product_ids_with_extended_search(database: Database):
+    invalid_product_ids = database.find_invalid_product_ids(advanced_search=True)
+    assert sum(invalid_product_ids) == 4174379265
