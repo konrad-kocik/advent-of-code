dfee225b5a0ff6432ba22a2b3c14e44bb714c0a6
Konrad Kocik <konrad.kocik@gmail.com>
2025-12-04 19:50:04 +0100

    Add 2025 day 4 part 2

--- 2025/day_4_printing_department/printing_department.py
+++ 2025/day_4_printing_department/printing_department.py
@@ -1,5 +1,6 @@
 class PrintingDepartment:
     ROLL_OF_PAPER = '@'
+    EMPTY_CELL = '.'
 
     def __init__(self):
         self._map: list[list[str]] = []
@@ -17,6 +18,26 @@ class PrintingDepartment:
                 accessable_rolls_of_paper_count += 1 if cell == self.ROLL_OF_PAPER and self._is_accessable(x, y) else 0
 
         return accessable_rolls_of_paper_count
+
+    def remove_rolls_of_paper_until_inaccessable(self) -> int:
+        removed_rolls_of_paper_count = 0
+
+        while accessable_rolls_of_paper_coords := self._get_accessable_rolls_of_paper_coords():
+            for x, y in accessable_rolls_of_paper_coords:
+                self._remove_roll_of_paper(x, y)
+                removed_rolls_of_paper_count += 1
+
+        return removed_rolls_of_paper_count
+
+    def _get_accessable_rolls_of_paper_coords(self) -> list[tuple[int, int]]:
+        accessable_rolls_of_paper_coords = []
+
+        for y, row in enumerate(self._map):
+            for x, cell in enumerate(row):
+                if cell == self.ROLL_OF_PAPER and self._is_accessable(x, y):
+                    accessable_rolls_of_paper_coords.append((x, y))
+
+        return accessable_rolls_of_paper_coords
 
     def _is_accessable(self, x: int, y: int) -> bool:
         neighbouring_rolls_of_paper_count = 0
@@ -39,3 +60,6 @@ class PrintingDepartment:
     def _get_cell(self, x: int, y: int) -> str | None:
         if y >= 0 and y < len(self._map) and x >= 0 and x < len(self._map[y]):
             return self._map[y][x]
+
+    def _remove_roll_of_paper(self, x: int, y: int):
+        self._map[y][x] = self.EMPTY_CELL
--- 2025/day_4_printing_department/test_printing_department.py
+++ 2025/day_4_printing_department/test_printing_department.py
@@ -1,8 +1,20 @@
+from pytest import fixture
+
 from printing_department import PrintingDepartment
 
 
-def test_count_accessable_rolls_of_paper():
-    printing_department = PrintingDepartment()
-    printing_department.load_map('test_input.raw')
+@fixture
+def printing_department():
+    pd = PrintingDepartment()
+    pd.load_map('test_input.raw')
+    return pd
+
+
+def test_count_accessable_rolls_of_paper(printing_department: PrintingDepartment):
     accessable_rolls_of_paper_count = printing_department.count_accessable_rolls_of_paper()
     assert accessable_rolls_of_paper_count == 13
+
+
+def test_remove_rolls_of_paper_until_inaccessable(printing_department: PrintingDepartment):
+    removed_rolls_of_paper_count = printing_department.remove_rolls_of_paper_until_inaccessable()
+    assert removed_rolls_of_paper_count == 43
