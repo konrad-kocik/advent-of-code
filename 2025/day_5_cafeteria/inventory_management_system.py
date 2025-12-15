c3bd75c28a017f73205b90ade82953f068006586
Konrad Kocik <konrad.kocik@gmail.com>
2025-12-15 20:16:22 +0100

    Add 2025 day 5 part 2

--- 2025/day_5_cafeteria/inventory_management_system.py
+++ 2025/day_5_cafeteria/inventory_management_system.py
@@ -1,6 +1,6 @@
 class InventoryManagementSystem:
     def __init__(self):
-        self._fresh_ingredient_ranges: list[range] = []
+        self._fresh_ingredient_ranges: list[tuple[int, int]] = []
         self._ingredients: list[int] = []
 
     def load_database(self, input_file_path: str):
@@ -16,8 +16,7 @@ class InventoryManagementSystem:
 
                 if section == 'ranges':
                     range_start, range_end = line.split('-')
-                    self._fresh_ingredient_ranges.append(range(int(range_start), 
-                                                               int(range_end) + 1))
+                    self._fresh_ingredient_ranges.append((int(range_start), int(range_end)))
                     
                 if section == 'ingredients':
                     self._ingredients.append(int(line))
@@ -26,7 +25,39 @@ class InventoryManagementSystem:
         fresh_ingredients_count = 0
     
         for ingredient in self._ingredients:
-            if any([ingredient in fresh_range for fresh_range in self._fresh_ingredient_ranges]):
+            if any([ingredient in range(fresh_range[0], fresh_range[1] + 1) for fresh_range in self._fresh_ingredient_ranges]):
                 fresh_ingredients_count += 1
 
         return fresh_ingredients_count
+
+    def count_all_possible_fresh_ingredients(self) -> int:
+        self._merge_overlaping_fresh_ingredient_ranges()
+        return self._count_ingredients_in_all_fresh_ranges()
+
+    def _merge_overlaping_fresh_ingredient_ranges(self):
+        should_merge = True
+
+        while should_merge:
+            merged_fresh_ingredient_ranges = []
+            should_merge = False
+
+            for fresh_range in self._fresh_ingredient_ranges:
+                for merged_fresh_range in merged_fresh_ingredient_ranges:
+                    if (fresh_range[0] in range(merged_fresh_range[0], merged_fresh_range[1] + 1) 
+                        or fresh_range[1] in range(merged_fresh_range[0], merged_fresh_range[1] + 1)
+                        or merged_fresh_range[0] in range(fresh_range[0], fresh_range[1] + 1)
+                        or merged_fresh_range[1] in range(fresh_range[0], fresh_range[1] + 1)):
+                        new_fresh_range_start = min(fresh_range[0], merged_fresh_range[0])
+                        new_fresh_range_end = max(fresh_range[1], merged_fresh_range[1])
+                        new_fresh_range = (new_fresh_range_start, new_fresh_range_end)
+                        merged_fresh_ingredient_ranges.append(new_fresh_range)
+                        merged_fresh_ingredient_ranges.remove(merged_fresh_range)
+                        should_merge = True
+                        break
+                else:
+                    merged_fresh_ingredient_ranges.append(fresh_range)
+
+            self._fresh_ingredient_ranges = merged_fresh_ingredient_ranges
+
+    def _count_ingredients_in_all_fresh_ranges(self) -> int:
+        return sum([len(range(fresh_range[0], fresh_range[1] + 1)) for fresh_range in self._fresh_ingredient_ranges])
--- 2025/day_5_cafeteria/test_inventory_management_system.py
+++ 2025/day_5_cafeteria/test_inventory_management_system.py
@@ -1,8 +1,36 @@
+from pytest import fixture, mark
+
 from inventory_management_system import InventoryManagementSystem
 
 
-def test_count_fresh_ingredients():
-    inventory_management_system = InventoryManagementSystem()
-    inventory_management_system.load_database('test_input.raw')
+@fixture
+def inventory_management_system():
+    ims = InventoryManagementSystem()
+    ims.load_database('test_input.raw')
+    return ims
+
+
+def test_count_fresh_ingredients(inventory_management_system: InventoryManagementSystem):
     fresh_ingredients_count = inventory_management_system.count_fresh_ingredients()
     assert fresh_ingredients_count == 3
+
+
+def test_count_all_possible_fresh_ingredients(inventory_management_system: InventoryManagementSystem):
+    all_possible_fresh_ingredients = inventory_management_system.count_all_possible_fresh_ingredients()
+    assert all_possible_fresh_ingredients == 14
+
+
+@mark.parametrize('range_1, range_2, expected_ranges', [((102, 105), (106, 110), [(102, 105), (106, 110)]),
+                                                        ((102, 105), (105, 110), [(102, 110)]),
+                                                        ((102, 105), (100, 102), [(100, 105)]),
+                                                        ((102, 105), (104, 115), [(102, 115)]),
+                                                        ((102, 105), (100, 104), [(100, 105)]),
+                                                        ((102, 110), (105, 106), [(102, 110)]),
+                                                        ((105, 106), (102, 110), [(102, 110)])])
+def test__merge_overlaping_fresh_ingredient_ranges(inventory_management_system: InventoryManagementSystem,
+                                                   range_1: tuple[int, int],
+                                                   range_2: tuple[int, int],
+                                                   expected_ranges: list[tuple[int, int]]):
+    inventory_management_system._fresh_ingredient_ranges = [range_1, range_2]
+    inventory_management_system._merge_overlaping_fresh_ingredient_ranges()
+    assert inventory_management_system._fresh_ingredient_ranges == expected_ranges
